ab05d49437d2daba423278faa5313c87afe7d867
SebastianErfort <55584035+SebastianErfort@users.noreply.github.com>
2022-01-04 17:46:21 +0100

    Create r10k.pp

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,13 @@
+class profile::r10k {
+  class { 'r10k':
+    remote => 'https://github.com/Sebastian/Erfort/puppet_control',
+  }
+  class {'r10k::webook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class { 'r10k::webhook':
+    user  => root,
+    group => root,
+  }
+}
